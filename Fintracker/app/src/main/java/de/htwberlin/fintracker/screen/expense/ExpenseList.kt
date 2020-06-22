705c3877a7641f4d303f7eb2adbcae9049a2711e
Michael Reno <s0565907@htw-berlin.de>
2020-06-22 19:08:20 +0200

    Added todo lists on expenselist.kt

--- Fintracker/app/src/main/java/de/htwberlin/fintracker/screen/expense/ExpenseList.kt
+++ Fintracker/app/src/main/java/de/htwberlin/fintracker/screen/expense/ExpenseList.kt
@@ -18,8 +18,8 @@ class ExpenseList : Fragment() {
     private lateinit var binding: FragmentExpenseListBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
+        // Inflate view and obtain an instance of the binding class
         binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_expense_list,
@@ -27,7 +27,19 @@ class ExpenseList : Fragment() {
             false
         )
 
+        // TODO: Initialise ViewModel with ViewModelProvider
+
+
+        // TODO: call methods to show expenses database
+
         return binding.root
     }
+
+    // Methods
+
+    // TODO: Methods for button pressed
+
+
+    // TODO: Database related
 
 }
