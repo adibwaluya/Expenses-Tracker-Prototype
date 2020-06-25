b54c7a6a9551abbed32f380ef450e8a7233f8118
Michael Reno <s0565907@htw-berlin.de>
2020-06-26 00:56:13 +0200

    Declared and initialised repository in ExpenseListViewModel.kt along with function to insert new expenses data to the database

--- Fintracker/app/src/main/java/de/htwberlin/fintracker/screen/expense/ExpenseListViewModel.kt
+++ Fintracker/app/src/main/java/de/htwberlin/fintracker/screen/expense/ExpenseListViewModel.kt
@@ -1,24 +1,43 @@
 package de.htwberlin.fintracker.screen.expense
 
+import android.app.Application
 import android.util.Log
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import de.htwberlin.fintracker.FintrackerRoomDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
 
-class ExpenseListViewModel : ViewModel() {
-    // Added logs to track when VM is initialised and destroyed
+@InternalCoroutinesApi
+class ExpenseListViewModel(application: Application) : AndroidViewModel(application) {
+
+    // Add repository to VM
+    private val repository: ExpenseListRepository
+
+    val allExpenses: LiveData<List<ExpenseData>>
+
+    // Add logs to track when VM is initialised and destroyed
     init {
         Log.i("ExpenseListViewModel", "ExpenseListVM created!")
         // TODO: create reference to ExpenseListDao through FinTrackerDatabase
-
+        val expenseListDao = FintrackerRoomDatabase.getDatabase(application).expenseListDao()
 
         // TODO: do we need repository?
-
+        repository = ExpenseListRepository(expenseListDao)
 
         // TODO: create a list of Expenses using LiveData
+        allExpenses = repository.allExpenses
     }
+
     override fun onCleared() {
         super.onCleared()
         Log.i("ExpenseListViewModel", "ExpenseListVM destroyed!")
     }
 
-    // TODO: Launch a coroutine to insert the data in a non-blocking way with a function
+    // Launch a coroutine to insert the data in a non-blocking way with a function
+    fun insert(expenseData: ExpenseData) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(expenseData)
+    }
 }
