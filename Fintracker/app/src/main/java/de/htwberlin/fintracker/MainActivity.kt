f7b9fc2fea37ad070e9be79c0125b721f0409c6f
Michael Reno <s0565907@htw-berlin.de>
2020-06-29 17:30:21 +0200

    Created RecyclerView Adapter

--- Fintracker/app/src/main/java/de/htwberlin/fintracker/MainActivity.kt
+++ Fintracker/app/src/main/java/de/htwberlin/fintracker/MainActivity.kt
@@ -2,11 +2,20 @@ package de.htwberlin.fintracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import de.htwberlin.fintracker.screen.expense.ExpenseListAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Add RecyclerView
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview_expenses)
+        val adapter = ExpenseListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
