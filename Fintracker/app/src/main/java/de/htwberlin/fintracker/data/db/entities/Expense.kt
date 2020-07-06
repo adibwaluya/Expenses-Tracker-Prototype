42fb9dfb461cb15827c07447aa319b69ca768af5
Michael Reno <s0565907@htw-berlin.de>
2020-07-06 21:33:59 +0200

    Added Expense data class

---
+++ Fintracker/app/src/main/java/de/htwberlin/fintracker/data/db/entities/Expense.kt
@@ -0,0 +1,17 @@
+package de.htwberlin.fintracker.data.db.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+const val EXPENSE_ID = 0
+@Entity(tableName = "List of Expenses")
+data class Expense(
+    // Entities for ROOM Database
+    var expInfo: String? = null,
+    var expDate: LocalDate? = null,
+    var expValue: Double? = null
+){
+    @PrimaryKey(autoGenerate = true)
+    var expId: Int = CURRENT_USER_ID
+}
