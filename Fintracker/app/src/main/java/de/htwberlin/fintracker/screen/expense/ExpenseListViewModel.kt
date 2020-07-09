548d22b85dd9eda4161bc91cc3fadb32bda1d65b
Michael Reno <s0565907@htw-berlin.de>
2020-07-09 18:20:50 +0200

    Reinstated ViewModel in directory

---
+++ Fintracker/app/src/main/java/de/htwberlin/fintracker/screen/expense/ExpenseListViewModel.kt
@@ -0,0 +1,10 @@
+package de.htwberlin.fintracker.screen.expense
+
+import androidx.lifecycle.ViewModel
+import de.htwberlin.fintracker.data.db.ExpenseDAO
+
+class ExpenseListViewModel(
+    private val dao: ExpenseDAO
+): ViewModel() {
+    fun getAllExpenses() = dao.getAllExpenses()
+}
