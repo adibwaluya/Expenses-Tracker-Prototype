e224e150e1beded6937dfc068a14b5f6d06659fa
s0567271 <adib.waluya@student.htw-berlin.de>
2020-07-09 17:23:37 +0200

    troubleshooting

--- Fintracker/app/src/main/java/de/htwberlin/fintracker/util/Exceptions.kt
+++
@@ -1,6 +0,0 @@
-package de.htwberlin.fintracker.util
-
-import java.io.IOException
-
-class ApiExceptions(message: String) : IOException(message)
-class NoInternetException(message: String) : IOException(message)
--- Fintracker/app/src/main/java/de/htwberlin/fintracker/util/ViewUtils.kt
+++
@@ -1,9 +0,0 @@
-package de.htwberlin.fintracker.util
-
-import android.content.Context
-import android.widget.Toast
-
-// Extension functions
-fun Context.toast(message: String){
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
