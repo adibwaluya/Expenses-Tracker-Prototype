b6355263910cd2446fd68bb748a53f53a781e88b
s0567271 <adib.waluya@student.htw-berlin.de>
2020-07-12 15:36:32 +0200

    Add an up button

--- Fintracker/app/src/main/java/de/htwberlin/fintracker/MainActivity.kt
+++ Fintracker/app/src/main/java/de/htwberlin/fintracker/MainActivity.kt
@@ -2,6 +2,8 @@ package de.htwberlin.fintracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 // import de.htwberlin.fintracker.screen.expense.ExpenseListAdapter
@@ -12,10 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
         // Add RecyclerView
         /* val recyclerView = findViewById<RecyclerView>(R.id.recyclerview_expenses)
         val adapter = ExpenseListAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this) */
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
